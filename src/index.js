44d0131a723b9d8b115c5612d7cb0bd0dde4f589
Pipz Padilla <pipzpadilla@gmail.com>
2017-02-08 21:10:08 +0800

    edited-bootstrap-file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
+import './assests/bootstrap.css';
 import './assests/style.css';
 
 import App from './components/App';
